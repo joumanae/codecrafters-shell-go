2c5c59f65d3ec8b29bb3638794759cd9700ff713
Joumana El Alaoui <joumana.codes@gmail.com>
2025-01-07 11:59:39 -0600

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -23,11 +23,17 @@ func main() {
 		if strings.HasPrefix(cmd, "echo") {
 			cmd = "echo"
 		}
+		if strings.HasPrefix(cmd, "type") {
+			cmd = "type"
+		}
+
 		switch cmd {
 		case "exit 0":
 			os.Exit(0)
 		case "echo":
 			fmt.Printf("%v", EchoCommand(line))
+		case "type":
+			fmt.Printf("%v", IsShellBuiltin(line))
 		default:
 			fmt.Printf("%s: command not found \n", cmd)
 
@@ -40,4 +46,20 @@ func main() {
 func EchoCommand(cmd string) string {
 	resp := strings.TrimPrefix(cmd, "echo ")
 	return resp
+}
+
+func IsShellBuiltin(cmd string) string {
+
+	builtin := strings.TrimSpace(strings.TrimPrefix(cmd, "type "))
+
+	builtins := map[string]bool{
+		"echo": true,
+		"exit": true,
+		"type": true,
+	}
+
+	if builtins[builtin] {
+		return fmt.Sprintf("%s is a shell builtin\n", builtin)
+	}
+	return fmt.Sprintf("%s: not found\n", builtin)
 }
